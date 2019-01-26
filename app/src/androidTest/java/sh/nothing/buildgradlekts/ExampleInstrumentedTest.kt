163e7e18018f10f58a1ca2f5c07c2c0654e67aa8
tnj <kitakore@gmail.com>
2019-01-26 17:19:25 +0900

    initial commit

---
+++ app/src/androidTest/java/sh/nothing/buildgradlekts/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package sh.nothing.buildgradlekts
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("sh.nothing.buildgradlekts", appContext.packageName)
+    }
+}
