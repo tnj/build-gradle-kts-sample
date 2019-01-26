0e1a1718e7ba1231a072cdda4cec545a973eacba
tnj <kitakore@gmail.com>
2019-01-26 22:42:56 +0900

    Convert `dependencies {}` block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,3 +23,15 @@ android {
         }
     }
 }
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${rootProject.extra["kotlin_version"]}")
+    implementation("androidx.appcompat:appcompat:1.0.0-beta01")
+    implementation("androidx.core:core-ktx:1.1.0-alpha03")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("com.google.android.material:material:1.0.0-beta01")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.0-alpha4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0-alpha4")
+}
