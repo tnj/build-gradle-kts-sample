d7eef4f6c7ff85ea7270c509d926e1dd7dc8a9f2
tnj <kitakore@gmail.com>
2019-01-26 22:25:15 +0900

    Convert `android` section into Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,3 +5,21 @@ plugins {
 }
 
 apply(from = "original.build.gradle")
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "sh.nothing.buildgradlekts"
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes.invoke {
+        "release" {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
+
+    // Groovy の場合、以下で `extra` に `kotlin_version` をセットできる
+    //     ext.kotlin_version = "1.3.11"
+    // これは以下と等価
+    //     (rootProject.)extensions.extraProperties.set("kotlin_version", "1.3.11")
+    // Gradle の世界では以下が成立する (= project自体のプロパティのように参照できる)
+    //     ext.kotlin_version == (rootProject.)kotlin_version ==  "1.3.11"
+
+    // Kotlin DSL では `ext` は `extra` になるが、 Kotlin では以下のように書くことはできない
+    //     extra.kotlin_version = "1.3.11"
+    // 明示的に `set` を呼ぶことはできるが、 type safe じゃないし補完も効かない
+    //     extra.set("kotlin_version", "1.3.11")  // `get` 時に `String?` へのキャストが必須
+    // 委譲プロパティを使って通常の変数のようにアクセスできる拡張を用意してくれている
+    // 以下のように書くと、 extra に set しつつ、その後も変数として参照できる
     val kotlin_version by extra("1.3.11")
+
     repositories {
         google()
         jcenter()
